a19671abaffcdc1ce9008122ffdced1d8bc5fe1d
Blecyrajan <84286020+Blecyrajan@users.noreply.github.com>
2021-06-07 20:12:21 +0530

    Update TASK1.py

--- TASK1.py
+++ TASK1.py
@@ -1,4 +1,4 @@
-celsius=float(input('Enter temperature in Celcius:'))
+celsius=float(input('Enter temperature in  degree Celcius:'))
 #calculate temperature in Fahrenheit
 fahrenheit=(celsius*1.8)+32
-print('%0.1f Celsius is equal to %0.1f degree Fahrenheit'%(celsius,fahrenheit))
+print('Equivalent Fahrenheit temperature is %0.1f degree Fahrenheit'%(fahrenheit))
